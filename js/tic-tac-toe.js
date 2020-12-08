f990ca94f6377de98a39d93adf8e5d443aa9c2dd
raecharles <16474572+raecharles@users.noreply.github.com>
2020-12-08 11:44:06 -0800

    winner / tie views
    +refactor;
    inite gameboard as object vs. orig array (avoid loop)

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -2,75 +2,56 @@
 const APP = document.querySelector('#app');
 
 const Gameboard = (() => {
-    let _gameboard = [];
+    let _gameboard = 
+        {
+            X:
+            {rowA: [],
+            rowB: [],
+            rowC: [],
+            col0: [],
+            col1: [],
+            col2: []}
+        ,
+            O: {rowA: [],
+            rowB: [],
+            rowC: [],
+            col0: [],
+            col1: [],
+            col2: []}
+        };
 
-    const play = (sqId) => {
+    const updateBoard = (sqId,type) => {
         let row = 'row' + sqId.substring(0,1).toUpperCase();
         let col = 'col' + sqId.substring(1);
-        let type = Game.getCurrentPlayer();
-        //need to set player turn state
-        _gameboard.forEach((moves,idx) => {
-            console.log(moves);
-            if (moves.hasOwnProperty(type))
-            {
-                //console.log(moves[type])
-                if (moves[type][row].indexOf(sqId) == -1)
-                {
-                    moves[type][row].push(sqId);
-                    moves[type][col].push(sqId);
-                    Display.setMarker(sqId,type);  
-                    //console.log(_gameboard);                
-                }
-            }
-        });
-    }
-    const checkWinner = (type) => {
-        let win = false;
-        var player = '';
-        _gameboard.filter(moves => {
-            if (moves.hasOwnProperty(type))
-            {
-                player = moves[type].name;
-                //diagonal winner
-                if (moves[type].rowB.indexOf('b1') != -1)
-                {
-                    win = (moves[type].rowA.indexOf('a0') != -1 && moves[type].rowC.indexOf('c2') != -1) ? true : (moves[type].rowA.indexOf('a2') != -1 && moves[type].rowC.indexOf('c0') != -1) ? true : false;
-                }
-                
-                //horizontal winner
-                Object.keys(moves[type]).map(key => {
-                    if (key != 'name' && moves[type][key].length == 3)
-                    {
-                    win = true;
-                    console.log(moves[type][key]);
-                    }
-                });
-            }
-        });
-        if (win) alert(player + ' wins!');
-    }
-    const initPlayers = (players) => {
-        Game.setCurrentPlayer(players.player1.type);
-        Object.keys(players).forEach((key) => {
-            //let k = players[key].name;
-            let player = {};
-            player[players[key].type] = {
-                rowA: [],
-                rowB: [],
-                rowC: [],
-                col0: [],
-                col1: [],
-                col2: [],
-                name: players[key].name //move to different state / tracke
-            };
-            _gameboard.push(player);
-        });
-        console.log(_gameboard);
+
+        if (_gameboard.X[row].indexOf(sqId) == -1 && _gameboard.O[row].indexOf(sqId) == -1)
+        {
+            _gameboard[type][row].push(sqId);
+            _gameboard[type][col].push(sqId);        
+            return true;
+        }
+        return false;
     }
     const resetBoard = () => {
-        _gameboard = [];
+        _gameboard = {
+            X: {rowA: [],
+            rowB: [],
+            rowC: [],
+            col0: [],
+            col1: [],
+            col2: []}
+        ,
+        
+            O: {rowA: [],
+            rowB: [],
+            rowC: [],
+            col0: [],
+            col1: [],
+            col2: []}
+        };
         console.log(_gameboard);
         drawBoard();
+        document.querySelector('#display').innerHTML = 'Enter Names.';
     }
     const getGameboard = () => {
         return _gameboard;
@@ -83,7 +64,9 @@ const Gameboard = (() => {
             let id = (x < 3) ? 'a' + x : (x < 6) ? 'b' + (x - 3) : 'c' + (x - 6);
             let box = createElemWithAttributes('div',{'id': id, 'class': 'box'});
             box.addEventListener('mouseup',(e) => {
-                play(e.target.id);          
+                /*let player = Game.getCurrentPlayer();
+                updateBoard(e.target.id,player.type);          */
+                Game.play(e.target.id);
             });
             document.querySelector('#gameboard').append(box);
             x++;
@@ -92,12 +75,12 @@ const Gameboard = (() => {
 
     const render = (selector) => {
         console.log('building gameboard...');
-        let gb = createElemWithAttributes('div', {'id': 'gameboard'});
+        let gb = createElemWithAttributes('div', {'id': 'gameboard', 'class': 'gameOver'});
         APP.append(gb);
         drawBoard();
     };
     return {
-        render, getGameboard, resetBoard, initPlayers, checkWinner
+        render, getGameboard, updateBoard, resetBoard
     }
 })();
 
@@ -109,11 +92,6 @@ const Player = (name,type) => {
 
 const Display = ((selector) => {
 
-    const setMarker = (id,marker) => {
-        document.getElementById(id).innerHTML = marker;
-        Game.setCurrentPlayer((marker == 'X') ? 'O' : 'X'); 
-        Gameboard.checkWinner(marker);
-    }
     /*///////////////////
     // MESSAGE DISPLAY
     ///////////////////*/
@@ -155,18 +133,15 @@ const Display = ((selector) => {
             let name = e.target.innerHTML.toLowerCase();
             if (name == 'start')
             {
-                let p1Name = document.querySelector('#player1').value;
-                let p2Name = document.querySelector('#player2').value;
-                //create players, init _gameboard
-                let player1 = Player(p1Name, 'X');
-                let player2 = Player(p2Name,'O');
-                Gameboard.initPlayers({player1,player2});
+                Game.startGame();                
             }
             else
             {
-                Gameboard.resetBoard();
-                Game.setCurrentPlayer('X');
+                /*Gameboard.resetBoard();
+                Game.setCurrentPlayer();*/
+                Game.resetGame();
             }
+            //document.querySelector('#gameboard').classList = '';
             e.target.innerHTML = (name == 'start') ? 'Restart' : 'Start';
         });
         
@@ -188,22 +163,112 @@ const Display = ((selector) => {
     }
 
     return {
-        setMarker, render
+        render
     }
     
 })('#app');
 
 const Game = (() => {
-    let _currentPlayer = '';
-    const setCurrentPlayer = (type) => {
-        _currentPlayer = type;
+    let _Players = {};
+    let _GameOver = false;
+    const setCurrentPlayer = () => {
+        _Players.currentPlayer = (_Players.currentPlayer == _Players.p1) ? _Players.p2 : _Players.p1;
+        document.querySelector('#display').innerHTML = _Players.currentPlayer.name + "'s move."; 
     }
     const getCurrentPlayer = () => {
-        return _currentPlayer;
+        return _Players.currentPlayer;
+    }
+    const setMarker = (id,marker) => {
+        document.getElementById(id).innerHTML = marker;
+        Game.checkWinner();
+        Game.setPlayers(); 
+    }
+    const startGame = () => {
+        let p1Name = document.querySelector('#player1').value;
+        let p2Name = document.querySelector('#player2').value;
+        _Players.p1 = Player(p1Name,'X');
+        _Players.p2 = Player(p2Name,'O');
+        _Players.currentPlayer = _Players.p1;
+        document.querySelector('#display').innerHTML = p1Name + "'s move."; 
+        document.querySelector('#gameboard').classList = '';
+        _GameOver = false;
+    }
+    const play = (id) => {
+        let type = _Players.currentPlayer.type;
+        if(Gameboard.updateBoard(id,type))
+        {
+            document.getElementById(id).innerHTML = type;
+            checkWinner();
+            if (!_GameOver) setCurrentPlayer(); 
+        }
+    }
+    const checkWinner = () => {
+        let win = false;
+        let player = _Players.currentPlayer;
+        let type = player.type;
+        let gb = Gameboard.getGameboard();
+        let moves = 0;
+        
+        //diag winner
+        if (gb[type].rowB.indexOf('b1') != -1)
+        {
+            if (gb[type].rowA.indexOf('a0') != -1 && gb[type].rowC.indexOf('c2') != -1) 
+            {
+                win = true;
+                highlight(['a0','b1','c2']);
+            }
+            else if (gb[type].rowA.indexOf('a2') != -1 && gb[type].rowC.indexOf('c0') != -1) 
+            {
+                win = true;
+                highlight(['a2','b1','c0']);
+            }
+        }
+        
+        //hor & vert winner
+        Object.keys(gb[type]).map(key => {
+            if (key != 'name')
+            {
+                let ct = gb[type][key].length;
+                moves += ct;
+                if (ct == 3)
+                {
+                    win = true;
+                    highlight(gb[type][key]);
+                }
+            }
+        });
+        
+        if (win) 
+        {
+            document.querySelector('#display').innerHTML = player.name + ' wins!';
+            _GameOver = true;
+        }
+        else if (moves > 9)
+        {
+            document.querySelector('#display').innerHTML = 'Tie game!';
+            _GameOver = true;
+        }
+        if (_GameOver)
+        {
+            document.querySelector('#gameboard').classList = 'gameOver';
+        }
+    }
+    const highlight = (ids) => {
+        let timing = 400;
+        ids.filter(id => {
+            setTimeout(function(){
+                document.getElementById(id).classList.add('highlight');  
+            }, timing);
+            timing += 400;
+        });
+    }
+    const resetGame = () => {
+        Gameboard.resetBoard();
+        setCurrentPlayer();
     }
     Display.render();
     return {
-        setCurrentPlayer, getCurrentPlayer
+        startGame, setCurrentPlayer, getCurrentPlayer, checkWinner,  play, resetGame
     }
 })();
 
